39783c0e34f78089911b61abd36457d41eb28836
the-anisa <theabfanisa@gmail.com>
2025-08-07 21:52:31 +0600

    Color Theme Changed

--- db_config.php
+++ db_config.php
@@ -1,16 +1,13 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-?>
-
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "droppoint";
+$database = "Droppoint";
 
 $conn = new mysqli($host, $username, $password, $database);
 
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " .
+   $conn->connect_error);
 }
 ?>
